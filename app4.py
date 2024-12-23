25a09435fef57d65611a955799d3c94f425c9f46
brazneto1985 <braz.s.neto@gmail.com>
2024-12-22 23:26:52 -0300

    Inclusão das referências vizinhas

--- app4.py
+++ app4.py
@@ -17,18 +17,14 @@ csv_path = os.path.join(os.path.dirname(__file__), 'endereços.csv')
 # Tenta carregar o CSV com dados de instalação, medidor, MRU, endereço e coordenadas
 try:
     df = pd.read_csv(csv_path)
-    print("Colunas disponíveis:", df.columns)
-    # Convertendo colunas para string e removendo espaços extras
     df['instalação'] = df['instalação'].astype(str).str.strip()
     df['medidor'] = df['medidor'].astype(str).str.strip()
     df['MRU'] = df['MRU'].astype(str).str.strip()
     df['endereço'] = df['endereço'].astype(str).str.strip()
 except FileNotFoundError:
-    print("Erro: O arquivo endereços.csv não foi encontrado.")
     df = pd.DataFrame(columns=['instalação', 'medidor',
                       'MRU', 'endereço', 'latitude', 'longitude'])
 except KeyError:
-    print("Erro: Colunas esperadas não foram encontradas no arquivo CSV.")
     df = pd.DataFrame(columns=['instalação', 'medidor',
                       'MRU', 'endereço', 'latitude', 'longitude'])
 
@@ -42,7 +38,6 @@ def login():
         password = request.form.get("password")
         if users.get(username) == password:
             session['logged_in'] = True
-            # Adiciona o nome do usuário na sessão
             session['username'] = username
             flash("Login bem-sucedido!", "success")
             return redirect(url_for("index"))
@@ -55,7 +50,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    session.clear()  # Limpa todas as informações da sessão para maior segurança
+    session.clear()
     flash("Você saiu da sua conta.", "info")
     return redirect(url_for("login"))
 
@@ -66,7 +61,7 @@ def logout():
 def index():
     if not session.get('logged_in'):
         return redirect(url_for("login"))
-    return render_template("index.html", mapa_url=None, resultado=None, versao=app.config['VERSION'])
+    return render_template("index.html", mapa_url=None, resultado=None, referencias=None, versao=app.config['VERSION'])
 
 # Rota para processar a busca
 
@@ -81,30 +76,37 @@ def buscar():
         flash("Por favor, insira uma instalação ou medidor.", "error")
         return redirect("/")
 
-    # Verifica se as colunas necessárias estão presentes
     if {'instalação', 'medidor', 'MRU', 'endereço'}.issubset(df.columns):
-        # Convertendo todas as colunas de interesse para string, para evitar problemas com valores não string
-        df['instalação'] = df['instalação'].astype(str).str.strip()
-        df['medidor'] = df['medidor'].astype(str).str.strip()
-
-        # Busca no DataFrame, ignorando maiúsculas/minúsculas
         resultado = df[
-            df['instalação'].str.contains(entrada, case=False, na=False) |
-            df['medidor'].str.contains(entrada, case=False, na=False)
+            df['instalação'].str.lower().str.contains(entrada) |
+            df['medidor'].str.lower().str.contains(entrada)
         ]
 
         if not resultado.empty:
+            index_resultado = resultado.index[0]
             dados = {
                 "instalação": resultado.iloc[0]['instalação'],
                 "MRU": resultado.iloc[0]['MRU'],
                 "medidor": resultado.iloc[0]['medidor'],
                 "endereço": resultado.iloc[0]['endereço']
             }
-            latitude = resultado.iloc[0]['latitude']
-            longitude = resultado.iloc[0]['longitude']
-            mapa_url = f"https://www.google.com/maps?q={latitude},{longitude}"
+            latitude = resultado.iloc[0].get('latitude', None)
+            longitude = resultado.iloc[0].get('longitude', None)
+            mapa_url = f"https://www.google.com/maps?q={latitude},{longitude}" if latitude and longitude else None
+
+            inicio = max(0, index_resultado - 5)
+            fim = min(len(df), index_resultado + 6)
+            referencias = df.iloc[inicio:fim][[
+                'MRU', 'instalação', 'medidor', 'endereço']]
+
             flash("Localização encontrada!", "success")
-            return render_template("index.html", mapa_url=mapa_url, resultado=dados, versao=app.config['VERSION'])
+            return render_template(
+                "index.html",
+                mapa_url=mapa_url,
+                resultado=dados,
+                referencias=referencias.to_dict('records'),
+                versao=app.config['VERSION']
+            )
         else:
             flash("Instalação ou Medidor não encontrados.", "error")
             return redirect("/")
